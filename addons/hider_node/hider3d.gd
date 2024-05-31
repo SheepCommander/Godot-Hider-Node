8ead538ef1231cd22c1ce40f3f465c606d2d6f1d
ShepChromebook <93790365+ShepChromebook@users.noreply.github.com>
2024-05-31 13:27:43 -0700

    Update hider3d.gd

--- addons/hider_node/hider3d.gd
+++ addons/hider_node/hider3d.gd
@@ -1,55 +1,62 @@
 @tool
-extends EditorPlugin
+extends Node
+class_name Hider3D
 
-const NODE_NAME = "Hider3D"
 const GROUP_NAME = "@Hider3D"
-const HIDER_PATH = "res://addons/hider_node/hider3d.gd"
-const ICON_PATH = "res://addons/hider_node/hider.png"
 
-var selection : EditorSelection = EditorInterface.get_selection()
+@export var enabled := false
+
+enum GAMESTART {CurrentState, ShowAll, HideAll}
+enum EDITOR {WireFrame, CompletelyInvisible}
+## CurrentState: Leave nodes in their current state on game start
+## ShowAll: Show all sibling nodes & their children on game start
+## HideAll: Hide all sibling nodes & their children on game start
+@export var on_game_start : GAMESTART = GAMESTART.CurrentState
+## WireFrame: 3D nodes still show a wireframe
+## CompletelyInvisible: 3D nodes will be completely invisible
+@export var hide_mode : EDITOR = EDITOR.WireFrame
+
+## Do not hide node if it has this name
+@export var except_list_names : Array[String]
 
 
-func _enter_tree() -> void:
-	add_custom_type(NODE_NAME, "Node", preload(HIDER_PATH), preload(ICON_PATH))
-	selection.selection_changed.connect(_on_selection_changed)
+var hiding := false
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		add_to_group(GROUP_NAME)
+		hiding = true
+		_hide_nodes()
+	else:
+		match on_game_start:
+			GAMESTART.CurrentState:
+				pass # Nothing
+			GAMESTART.ShowAll:
+				_show_nodes()
+			GAMESTART.HideAll:
+				_hide_nodes()
+		queue_free()
 
 
-func _exit_tree() -> void:
-	remove_custom_type(NODE_NAME)
+func _process(delta: float) -> void:
+	pass
 
 
-func _on_selection_changed() -> void:
-	var root : Node = get_tree().edited_scene_root
-	var selected_nodes : Array[Node] = selection.get_selected_nodes()
-	
-	var hiders : Array[Node] = get_tree().get_nodes_in_group(GROUP_NAME)
-	if hiders.is_empty():
-		return # No hiders in scene, no need to do checks.
-	
-	for selected_node : Node in selected_nodes:
-		for hider : Hider3D in hiders:
-			if not hider.enabled:
-				continue # This hider is disabled. Skip.
-			var hider_path := str(root.get_path_to(hider.get_parent()))+"/"
-			var selection_path := str(root.get_path_to(selected_node))
-			if hider_path in selection_path:
-				print("show")
-				hider # Show nodes if selection is inside
-	#for node : Node in selected_nodes:
-		#for hider : Hider3D in hiders:
-			#if not hider.enabled:
-				#continue
-			#if (str(root.get_path_to(hider.get_parent()))+"/") in 
-				#(str(root.get_path_to(node))+"/"):
-				# Enable hider if 
-				#enabled_hiders.append(hider)
-				#hiders.erase(hider)
-	#for hider in hiders:
-		#if not hider.active:
-			#continue
-		#hider.hide_nodes()
-		#hider.active = false
-#
-	#for hider in enabled_hiders:
-		#hider.show_nodes()
-		#hider.active = true
+func _hide_nodes():
+	match hide_mode:
+		EDITOR.WireFrame:
+			_hide_nodes_wireframe()
+		EDITOR.CompletelyInvisible:
+			_hide_nodes_invisible()
+
+
+func _hide_nodes_wireframe():
+	pass
+
+
+func _hide_nodes_invisible():
+	pass
+
+
+func _show_nodes():
+	pass
